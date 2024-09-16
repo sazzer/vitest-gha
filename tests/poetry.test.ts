c43d0e41f52802e7b785ded656074e257ec62174
Graham Cox <graham@grahamcox.co.uk>
2024-09-16 16:48:14 +0100

    Removed the builder test so that it's BDD only

--- tests/poetry.test.ts
+++ tests/poetry.test.ts
@@ -1,12 +1,6 @@
 import { test } from "vitest";
 const { spec } = require("pactum");
 
-test("Builder", async () => {
-    await spec()
-        .get("https://poetrydb.org/title/Ozymandias/lines.json")
-        .expectStatus(123);
-});
-
 test("BDD", async () => {
     const _spec = spec();
     _spec.get("https://poetrydb.org/title/Ozymandias/lines.json");
