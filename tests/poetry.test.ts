7c838db698fd4077184a6a18932df2edd849a2bb
Graham Cox <graham@grahamcox.co.uk>
2024-09-16 16:47:25 +0100

    Removed the BDD test so that it's builder only

--- tests/poetry.test.ts
+++ tests/poetry.test.ts
@@ -7,9 +7,3 @@ test("Builder", async () => {
         .expectStatus(123);
 });
 
-test("BDD", async () => {
-    const _spec = spec();
-    _spec.get("https://poetrydb.org/title/Ozymandias/lines.json");
-    await _spec.toss();
-    _spec.response().to.have.status(123);
-});
